708293aca3f413e12d8f295da3cbb11b4426b532
Robin Kidder <34462537+richarro@users.noreply.github.com>
2018-02-20 08:55:58 +0900

    added updated admin income page

--- admin_post_income_expenses.php
+++ admin_post_income_expenses.php
@@ -1,5 +1,39 @@
 <!-- INSTRUCTIONS: this is the header and footer template for the primary ADMIN pages. Code your forms, tables, etc., below the navigation tags. Placeholders have been included where variables will be displayed based on session login information for the user. Leave these "AS IS" for now. To maintain consistency, please do not change the header information other than where indicated with additional comments. -->
 
+<?php
+
+require 'database.php';
+
+if(isset($_POST['mem_no'])) {
+	$pdo = Database::connect();
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+	$mem_no = $_POST['mem_no'];
+	$trans_type = $_POST['trans_type'];
+	$expense_type = $_POST['expense_type'];
+	$amount = $_POST['amount'];
+	$description = $_POST['description'];
+	$q = "INSERT INTO general_ledger (gen_led_users_mem_no, gen_led_transaction_type, gen_led_expense_type, gen_led_amount, gen_led_description) VALUES (:mem_no, :trans_type, :expense_type, :amount, :description)";
+	$q2 = "INSERT INTO balance (bal_gen_led_id) VALUES (LAST_INSERT_ID())";
+	$q3 = "INSERT INTO receipts (rec_gen_led_id) VALUES (LAST_INSERT_ID())";
+	$query = $pdo->prepare($q);
+	$query2 = $pdo->prepare($q2);
+	$query3 = $pdo->prepare($q3);
+	$result = $query->execute(array(
+	   ":mem_no" => $mem_no,
+	   ":trans_type" => $trans_type,
+	   ":expense_type" => $expense_type,
+	   ":amount" => $amount,
+	   ":description" => $description
+));
+	$query2->execute();
+	$query3->execute();
+		Database::disconnect();
+		header("Location: admin_view_general_ledger.php");
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -50,49 +84,66 @@
 <!-- IMPORTANT #3: insert/paste YOUR code below to create the table, form, etc. -->
 <div id="search-by">
 <h4>Search for Member by Name:</h4>
-</div>
+</div><br>
 
+<form action="" method="post">
 <div class="select-name">
-	<label>Member Name</label>
-	<select name="select-name">
-		<option value="">Select Last/First</option>
-		<?php
-			$query = "SELECT * FROM member;"
-		?>
-		
-		
-		<option value=""></option>
-		
+	<label>Member Name:</label>
+		<select name="mem_no">
+		<option value="">Select Last/First Name</option>
+		<option value="1">Kidder, Jason</option>
+		<option value="2">Jones, Catherine</option></div><br>
+		</select>
+</div><br>
+
+<div class="select-trans">
+	<label>Transaction Type:</label>
+	<select name="trans_type">
+		<option value="">Select Transaction Type</option>
+		<option value="I">Income</option>
+		<option value="E">Expense</option>
 	</select>
+</div><br>
+
+<div class="select-inc-exp-type">
+	<label>Income/Expense Type:</label>
+	<select name="expense_type">
+		<option value="">Select Type</option>
+		<option value="AD">I - Annual Dues</option>
+		<option value="RD">I - Renewal Dues</option>
+		<option value="FD">I - Fundraiser</option>
+		<option value="DO">I - Donation</option>
+		<option value="M">E - Meeting</option>
+		<option value="T">E - Trip Reimbursement</option>
+		<option value="O">E - Other</option>
+		<option value="S">E - Supplies</option>
+	</select>
+</div><br>
+
+<div class="amount">
+	<label>Amount:</label>
+	<input type="text" name="amount">
+</div><br>
+
+<div class="description">
+	<label>Description:</label>
+	<textarea type="text" rows="4" cols="20" wrap="soft" name="description"></textarea>
 </div>
 
+<div class="submit-inc-exp"><br>
+	<button type="submit" name="submit" class="submit-inc-exp">Submit</button><br><br><br>
+	<a class="btn" href="admin_main_dashboard.php">Back</a>
+	</div>
+	
+<div class="reset">
+	<button type="reset" value="Reset">Reset</button>
+</div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+</form>
 
 
 <!-- Page footer; please do not change. Footer should always be on the bottom of the page but not fixed. -->
 <footer>
 <p>This site is intended for personal use by the members of the Yokota Sportsmen&#39;s Club specifically for conducting club business. All rights reserved. Yokota Sportsmen&#39;s Club, Fussa-shi, Tokyo, Japan | Yokota Air Base, Tokyo, Japan</p>
+</body>
+</html>
