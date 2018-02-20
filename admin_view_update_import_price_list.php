9ffe9abc0c7584f47d4c283f3bfd7a1accbe4a0d
Nassir Abdalla <35644038+nabdalla617@users.noreply.github.com>
2018-02-19 20:53:51 -0500

    Add files via upload

--- admin_view_update_import_price_list.php
+++ admin_view_update_import_price_list.php
@@ -82,6 +82,12 @@
 </tbody>
 </table>
 <br><br><br><br><br><br>
+<div id="button">
+	<ul>
+		<li><a href="admin_report_quarterly_financial_statement">Print</a></li>
+		<li><a href="admin_main_dashboard.php">Return to Dashboard</a></li>
+	</ul>
+</div>
 <!-- Page footer; please do not change. Footer should always be on the bottom of the page but not fixed. -->
 <footer>
 <p>This site is intended for personal use by the members of the Yokota Sportsmen&#39;s Club specifically for conducting club business. All rights reserved. Yokota Sportsmen&#39;s Club, Fussa-shi, Tokyo, Japan | Yokota Air Base, Tokyo, Japan</p>
