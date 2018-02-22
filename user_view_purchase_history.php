67e293b37b0ff398d5f945964025a71b64e5180c
Robin Kidder <34462537+richarro@users.noreply.github.com>
2018-02-22 10:05:13 +0900

    corrected stylesheet link

--- user_view_purchase_history.php
+++ user_view_purchase_history.php
@@ -3,7 +3,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+	<link rel="stylesheet" type="text/css" href="css/new_master_stylesheet.css">
 	<title>Membership and Accounting System (MAS)</title>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
