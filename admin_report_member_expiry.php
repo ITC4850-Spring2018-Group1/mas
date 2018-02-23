82c36c546456589dc11932f6a94fa1c7a2eb1177
aghazioui <35120271+aghazioui@users.noreply.github.com>
2018-02-23 17:01:22 -0500

    Update admin_report_member_expiry.php

--- admin_report_member_expiry.php
+++ admin_report_member_expiry.php
@@ -70,10 +70,13 @@
 <br><br><br><br><br><br>
 <div id="button">
 	<ul>
-		  <li><a href="admin_report_member_expiry">Print</a></li>
-		  <li><a href="admin_main_dashboard.php">Return to Dashboard</a></li>
+		 <li><a href="admin_report_member_expiry.php">Print</a></li>
+		 <li><a href="admin_main_dashboard.php">Return to Dashboard</a></li>
 	</ul>
 </div>
+<br><br><br>
 <!-- Page footer; please do not change. Footer should always be on the bottom of the page but not fixed. -->
 <footer>
 <p>This site is intended for personal use by the members of the Yokota Sportsmen&#39;s Club specifically for conducting club business. All rights reserved. Yokota Sportsmen&#39;s Club, Fussa-shi, Tokyo, Japan | Yokota Air Base, Tokyo, Japan</p>
+</body>
+</html>
