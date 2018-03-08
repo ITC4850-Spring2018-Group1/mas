13eeec879e49d5fcf2c23cc7fbe7f357a24a9d9e
Christopher Camillucci <ccamm@CammoYOGA920.mit.edu>
2018-03-08 12:16:20 -0500

    Added red header session username to admin add new members

--- admin_add_new_members.php
+++ admin_add_new_members.php
@@ -1,3 +1,23 @@
+<br>
+<?php  
+session_start();
+if(isset($_SESSION["sess_username"]))  
+ {  
+	echo '<h6>Your session is currently ACTIVE '.$_SESSION["sess_username"].'</h6>';    
+ }  
+ else  
+ {  
+	header("location:index.php");  
+ }  
+  
+if( $_SESSION['sess_user_type'] == "A") {
+		
+		  }
+	else {
+		header('Location: index.php');
+		}
+
+?> 
 <!-- INSTRUCTIONS: this is the header and footer template for the primary USER pages. Code your forms, tables, etc., below the navigation tags. Placeholders have been included where variables will be displayed based on session login information for the user. Leave these "AS IS" for now. To maintain consistency, please do not change the header information other than where indicated with additional comments. -->
 
 <!DOCTYPE html>
@@ -7,6 +27,7 @@
 	<title>Membership and Accounting System (MAS)</title>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- need to add favicon links HERE -->
 </head>
 
 <body>
@@ -16,11 +37,11 @@
 
 <!-- this redirects the user to a signout page where the variables will be reset and the session terminated -->
 <div class="signout">
-	<a href="signout.php">Sign Out</a>	
+	<a href="logout.php">Sign Out</a>	
 </div>
 
 <div class="logininfo">
-	<p>[placeholder][placeholder] you are logged in as an ADMIN</p>
+	<?php echo '<p>Welcome ' . $_SESSION["sess_username"].'! You are logged in as an ADMIN</p>'; ?> 
 </div>
 <br>
 <br>
