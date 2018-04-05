c5cd83816810661b1cef51c7a7db80c9000d487d
Robin Kidder <34462537+richarro@users.noreply.github.com>
2018-04-05 19:52:58 +0900

    reupload

--- index.php
+++ index.php
@@ -61,6 +61,7 @@ if(isset($_POST["login"]))
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
  <link rel="stylesheet" type="text/css" href="css/new_master_stylesheet.css">
+ <link rel=""
  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
 </head>  
 <body>  
