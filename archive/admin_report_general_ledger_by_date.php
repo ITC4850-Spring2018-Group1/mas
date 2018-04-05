d4195b55ca19bc6e80e23ad2575d1166871c6f38
Robin Kidder <34462537+richarro@users.noreply.github.com>
2018-04-05 19:58:13 +0900

    added files for archive
    no longer needed due to datatables

---
+++ archive/admin_report_general_ledger_by_date.php
@@ -0,0 +1,128 @@
+<!-- INSTRUCTIONS: this is the header and footer template for the primary ADMIN pages. Code your forms, tables, etc., below the navigation tags. Placeholders have been included where variables will be displayed based on session login information for the user. Leave these "AS IS" for now. To maintain consistency, please do not change the header information other than where indicated with additional comments. -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+	<title>Membership and Accounting System (MAS)</title>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+<div class="main-heading">
+	<h1>Yokota Sportsmen&#39;s Club</h1>
+</div>
+
+<!-- this redirects the user to a signout page where the variables will be reset and the session terminated -->
+<div class="signout">
+	<a href="signout.php">Sign Out</a>	
+</div>
+
+<div class="logininfo">
+	<p>[placeholder][placeholder] you are logged in as an ADMIN</p>
+</div>
+<br>
+<br>
+<br>
+<br>
+<!-- IMPORTANT #1: change the links to the pages in which users should be directed for YOUR specific wireframe as well as the text to display on the button -->
+<div class="nav-admin">
+	<ul>
+		<li><a href="admin_add_new_members.php">Add Membership</a></li>
+		<li><a href="admin_view_update_membership_summary.php">View Membership</a></li>
+		<li><a href="admin_view_update_ATF_status.php">View ATF Status</a></li>
+		<li><a href="admin_view_update_import_price_list.php">Import Inventory</a></li>
+		<li><a href="admin_post_income_expenses.php">Post Income/Expenses</a></li>
+	</ul>
+<br>
+</div>
+<hr>
+<br>
+<div class="main-title">
+<h2>MEMBERSHIP AND ACCOUNTING SYSTEM</h2>
+</div><br>
+<!-- IMPORTANT #2: change the H3 tag to match the title of YOUR specific wireframe -->
+<div class="individual-page-title">	
+	<h3>General Ledger Report</h3>
+</div>
+<div class="individual-page-title">	
+	<h3>YYYY/MM/FF - YYYY/MM/DD</h3>
+</div>
+
+<!-- IMPORTANT #3: insert/paste YOUR code below to create the table, form, etc. -->
+<br><center>
+<table class="user-table"> 
+	<thead> 
+		 <th width="190px">Transaction Date</th> 
+		 <th width="130px">Receipt #</th> 
+		 <th width="230px">Description</th>
+		 <th width="130px">Transaction Type</th>
+		 <th width="130px">Expense</th> 
+		 <th width="130px">Income</th> 
+		 <th width="130px">Balance</th> 
+ 	</thead>
+ 	<tfoot>
+ 		<tr><td colspan="3" align="right">Total</td>
+ 			<td></td>
+ 			<td></td>
+ 			<td></td>
+			<td></td>
+ 		</tr></tfoot>
+
+<tbody>
+<?php
+ include 'database.php';
+ $pdo = Database::connect();
+ $sql = 'SELECT a.gen_led_trans_date, c.rec_receipt_no, a.gen_led_description, a.gen_led_transaction_type, a.gen_led_amount, a.gen_led_amount, b.bal_acct_balance
+FROM general_ledger a
+LEFT JOIN balance b ON a.gen_led_id=b.bal_gen_led_id
+LEFT JOIN receipts c ON a.gen_led_id=c.rec_gen_led_id
+LEFT JOIN ref_gen_led_expense_type d ON a.gen_led_expense_type=d.ref_gen_led_expense_typ
+LEFT JOIN ref_gen_led_transaction_type e ON a.gen_led_transaction_type=e.ref_gen_led_transaction_typ
+LEFT JOIN ref_gen_led_income_type f ON a.gen_led_income_type=f.ref_gen_led_income_typ
+LEFT JOIN member g ON a.gen_led_users_mem_no=g.mem_no
+LEFT JOIN balance h ON a.gen_led_id=h.bal_gen_led_id
+LEFT JOIN users i ON a.gen_led_add_by=i.user_mem_no';
+
+if ($_GET['sort'] == 'id')
+{
+	$sql .= " ORDER BY gen_led_id";
+}
+elseif ($_GET['sort'] == 'date')
+{
+	$sql .= " ORDER BY gen_led_trans_date";
+}
+elseif ($_GET['sort'] == 'mem_no')
+{
+	$sql .= " ORDER BY mem_no";
+}
+
+foreach ($pdo->query($sql) as $row) {
+					echo '<tr>';
+					echo '<td>'. $row['gen_led_trans_date'] . '</td>';
+					echo '<td>'. $row['rec_receipt_no'] . '</td>';
+					echo '<td>'. $row['gen_led_description'] . '</td>';
+					echo '<td>'. $row['gen_led_transaction_type'] . '</td>';
+					echo '<td>'. $row['gen_led_amount'] . '</td>';
+					echo '<td>'. $row['gen_led_amount'] . '</td>';
+					echo '<td>'. $row['bal_acct_balance'] . '</td>';
+					echo ' ';
+					echo '</tr>';
+ }
+ Database::disconnect();
+?>
+
+</tbody>
+</table>
+</center>
+<br><br><br><br><br><br>
+<div id="button">
+	<ul>
+		<li><a href="admin_report_quarterly_financial_statement">Print</a></li>
+		<li><a href="admin_main_dashboard.php">Return to Dashboard</a></li>
+	</ul>
+</div>
+<!-- Page footer; please do not change. Footer should always be on the bottom of the page but not fixed. -->
+<footer>
+<p>This site is intended for personal use by the members of the Yokota Sportsmen&#39;s Club specifically for conducting club business. All rights reserved. Yokota Sportsmen&#39;s Club, Fussa-shi, Tokyo, Japan | Yokota Air Base, Tokyo, Japan</p>
