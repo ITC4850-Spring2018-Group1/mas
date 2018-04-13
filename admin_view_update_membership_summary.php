9ad631fd6e94f5f248f6f55877bc5e79b725cb0a
Robin Kidder <34462537+richarro@users.noreply.github.com>
2018-04-13 14:40:21 +0900

    reviewed for updates

--- admin_view_update_membership_summary.php
+++ admin_view_update_membership_summary.php
@@ -161,8 +161,10 @@ foreach ($pdo->query($sql) as $row) {
 		<li><a href="admin_main_dashboard.php">Return to Dashboard</a></li>
 	</ul>
 </div>
-
-
+<br>
+<br>
+<br>
+<br>
 <!-- Page footer; please do not change. Footer should always be on the bottom of the page but not fixed. -->
 <footer>
 <p>This site is intended for personal use by the members of the Yokota Sportsmen&#39;s Club specifically for conducting club business. All rights reserved. Yokota Sportsmen&#39;s Club, Fussa-shi, Tokyo, Japan | Yokota Air Base, Tokyo, Japan</p>
